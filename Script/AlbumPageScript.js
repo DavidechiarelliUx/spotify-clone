b651cc298887757d1e078bf6c891bfc672ce0589
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-01-27 17:35:18 +0100

    .

---
+++ Script/AlbumPageScript.js
@@ -0,0 +1,15 @@
+const closeBtn = document.getElementById("close-btn");
+const aside2 = document.getElementById("aside2");
+closeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  aside2.style = "display:none";
+});
+
+const openAside = document.getElementById("openAside");
+
+openAside.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  aside2.style = "display:block";
+});
