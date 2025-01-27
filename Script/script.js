c37bd1f1fe0897e231283762c4e3f7f4847c3037
Davide Chiarelli <davidechiarelli@MacBookPro.homenet.telecomitalia.it>
2025-01-27 16:12:36 +0100

    add toggle aside2

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,16 @@
-console.log("ciao")
+const closeBtn=document.getElementById("close-btn");
+const aside2 = document.getElementById("aside2");
+closeBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    aside2.style="display:none";
+
+})
+
+const openAside=document.getElementById("openAside");
+
+openAside.addEventListener("click", (e)=>{
+    e.preventDefault();
+
+    aside2.style="display:block";
+})
